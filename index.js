fe524abfc361b478995c9b032318c49cc48dd871
Kevin Lacker <lacker@gmail.com>
2015-11-21 17:14:11 -0800

    isValidSpot and neighbors

--- index.js
+++ index.js
@@ -38,4 +38,27 @@ class Board {
 
     return true;
   }
+
+  // Returns whether [i, j] is a valid spot on the board.
+  isValidSpot(i, j) {
+    return (i >= 0 &&
+            i < size &&
+            j >= 0 &&
+            j < size);
+  }
+
+  // Returns a list of [i, j] pairs for all valid neighbors to the
+  // provided spot.
+  neighbors(i, j) {
+    let answer = [];
+    for ([a, b] of [[i + 1, j],
+                    [i - 1, j],
+                    [i, j + 1],
+                    [i, j - 1]]) {
+      if (this.isValidSpot(a, b)) {
+        answer.push([a, b]);
+      }
+    }
+    return answer;
+  }
 }
