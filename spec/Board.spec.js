c1ef7a8d9c135193ee88f8bfd0658f11fb0113a8
Kevin Lacker <lacker@gmail.com>
2015-11-24 08:43:11 -0500

    add another test

--- spec/Board.spec.js
+++ spec/Board.spec.js
@@ -2,8 +2,20 @@
 
 let Board = require('../Board');
 
+// TODO: do with import
+const BLACK = -1;
+const EMPTY = 0;
+const WHITE = 1;
+
 describe('Board', () => {
   it('can be created', () => {
     let board = new Board(19);
+  });
+
+  it('can find a group', () => {
+    let board = new Board(2);
+    board.makeMove(0, 0, BLACK);
+    board.makeMove(0, 1, BLACK);
+    expect(board.findGroup(0, 0).spots.length).toEqual(2);
   });
 });
