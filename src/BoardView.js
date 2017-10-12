806050647a713979a6bc182b8676e09b8a85f981
Kevin Lacker <lacker@gmail.com>
2017-10-12 10:50:38 -0700

    cell

--- src/BoardView.js
+++ src/BoardView.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import Board from './Board';
 
+function cell({color}) {
+  if (color === Board.BLACK) {
+    return <div className="Cell" backgroundColor="#000000" />;
+  }
+  if (color === Board.WHITE) {
+    return <div className="Cell" backgroundColor="#FFFFFF" />;
+  }
+  return <div className="Cell" backgroundColor="#777777" />;
+}
+
 class BoardView extends Component {
   render(props) {
     // TODO: expand props.grid
