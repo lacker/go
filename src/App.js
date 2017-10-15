cf0681e376a663d62322e66393a3984e634484b8
Kevin Lacker <lacker@gmail.com>
2017-10-15 15:41:56 -0700

    board setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import BoardView from './BoardView';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.board = new Board();
+    this.board = new Board(9);
+    this.board.makeMove(3, 3, Board.BLACK);
+    this.board.makeMove(2, 5, Board.WHITE);
   }
 
   render() {
