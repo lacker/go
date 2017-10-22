78f29fcedd24d8693ae921660a0ba1006ccfb099
Kevin Lacker <lacker@gmail.com>
2017-10-22 08:39:47 -0700

    shuffling round and still sucks

--- src/BoardView.js
+++ src/BoardView.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
 import Board from './Board';
 
+let cellStyle = {
+  height: 10,
+  width: 10,
+};
+
 let styles = {
   black: {
     backgroundColor: "#000000",
+    ...cellStyle,
   },
   white: {
     backgroundColor: "#FFFFFF",
+    ...cellStyle,
   },
   empty: {
     backgroundColor: "#777777",
+    ...cellStyle,
   },
-}
+};
 
 function cell({color}) {
   if (color === Board.BLACK) {
-    return <div className="Cell" style={styles.black} />;
+    return <div className="Cell" style={[styles.black, styles.cell]} />;
   }
   if (color === Board.WHITE) {
-    return <div className="Cell" style={styles.white} />;
+    return <div className="Cell" style={[styles.white, styles.cell]} />;
   }
-  return <div className="Cell" style={styles.empty} />;
+  return <div className="Cell" style={[styles.empty, styles.cell]} />;
 }
 
 function row(items) {
